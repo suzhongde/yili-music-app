6a46ceaa9771e3e6020c478ef2964654b0c6306a
suzhongde <2373544536@qq.com>
2022-04-24 06:20:58 +0800

    feat: add home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 
+import '../widget/header.dart';
+
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: ElevatedButton(
-            onPressed: () {
+        body: Column(
+          children: <Widget>[
+            Header(),
+            ElevatedButton(onPressed: () {
               Navigator.pushNamed(context, '/login');
-            },
-            child: const Text("登录"),
-          )
-      ),
+            }, child: Text("登录"),)
+          ],
+        )
     );
   }
 }
